1ed85316a1beabafd4647f9d9b9102a910a61026
ymatuhin <ym@ymatuhin.ru>
2017-12-11 14:48:00 +0300

    Fix redirect

--- assets/redirect.js
+++ assets/redirect.js
@@ -1,25 +1,27 @@
-var DEFAULT_LANG = 'en'
-var HOME_PAGE_LANG = 'ru'
+var DEFAULT_LANG = 'en';
+var HOME_PAGE_LANG = 'ru';
 var PATHES = {
-  "ru": "/",
-  "en": "/en/",
-  "uk": "/uk/",
-  "be": "/be/",
-}
+  ru: '/',
+  en: '/en/',
+  uk: '/uk/',
+  be: '/be/',
+};
 
 var userLang = navigator.language || navigator.userLanguage;
 var lang = userLang.split('-')[0].toLowerCase() || DEFAULT_LANG;
 var path = location.pathname.replace(/\//g, '') || HOME_PAGE_LANG;
 
 (function() {
+  if (!sessionStorage) return;
+
   if (lang == path) {
-    sessionStorage.redirected = true
-    return
-  };
+    sessionStorage.redirected = true;
+    return;
+  }
 
   if (sessionStorage.redirected) return;
   if (!PATHES[lang]) return;
 
   sessionStorage.redirected = true;
   location.pathname = PATHES[lang];
-})()
+})();
