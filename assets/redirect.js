2698ed959d3967dda85f067f75245d16db3c1de6
ymatuhin <ym@ymatuhin.ru>
2017-12-19 13:07:14 +0300

    Update likely, add try-catch

--- assets/redirect.js
+++ assets/redirect.js
@@ -1,24 +1,30 @@
-var DEFAULT_LANG = 'en';
-var HOME_PAGE_LANG = 'ru';
-var PATHES = {
-  ru: '/',
-  en: '/en/',
-  uk: '/uk/',
-  be: '/be/',
-};
-
-var userLang = navigator.language || navigator.userLanguage;
-var lang = userLang.split('-')[0].toLowerCase() || DEFAULT_LANG;
-var path = location.pathname.replace(/\//g, '') || HOME_PAGE_LANG;
+function checkSS() {
+  try {
+    sessionStorage.testWrite = '123';
+    return sessionStorage.testWrite === '123';
+  } catch (e) {
+    return false;
+  }
+}
 
 (function() {
-  if (!sessionStorage) return;
+  if (!checkSS()) return;
 
-  if (lang == path) {
-    sessionStorage.redirected = true;
-    return;
-  }
+  var HOME_PAGE_LANG = 'ru';
+  var PATHES = {
+    ru: '/',
+    en: '/en/',
+    uk: '/uk/',
+    be: '/be/',
+  };
 
+  var userLang = navigator.language || navigator.userLanguage;
+  var lang = userLang.split('-')[0].toLowerCase();
+  if (!lang) return;
+
+  var path = location.pathname.replace(/\//g, '') || HOME_PAGE_LANG;
+
+  if (lang == path) return (sessionStorage.redirected = true);
   if (sessionStorage.redirected) return;
   if (!PATHES[lang]) return;
 
